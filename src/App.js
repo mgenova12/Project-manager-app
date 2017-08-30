58ba4205beacd54aa337ab06f21b60ec2197ed3f
Marco <mgenova03@gmail.com>
2017-08-30 15:45:22 -0400

    add new project functionality

--- src/App.js
+++ src/App.js
@@ -29,11 +29,17 @@ class App extends Component {
     ]});
   }
 
+  handleAddProject(project){
+    // console.log(project)
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects:projects})
+  }
 
   render() {
     return (
       <div className="App">
-        <AddProject/>
+        <AddProject AddProject={this.handleAddProject.bind(this)}/>
         <Projects projects={this.state.projects}/>
       </div>
     );
